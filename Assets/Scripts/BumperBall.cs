87fd8232a120b4b9cf19337ae67c9a4f6f45d8ca
tylergreen1993 <tyler-green@live.com>
2021-07-09 17:11:39 -0500

    Got Bumper Ball working

--- Assets/Scripts/BumperBall.cs
+++ Assets/Scripts/BumperBall.cs
@@ -3,36 +3,11 @@ using UnityEngine;
 public class BumperBall : MonoBehaviour
 {
     public Rigidbody Rb;
-    public float Speed = 5;
-    public bool IsMoving;
-    public bool IsPlayer;
+    public float Speed = 50;
 
-    void FixedUpdate()
+    public void AddForce(Vector3 dir)
     {
-        if (IsPlayer)
-        {
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            {
-                Rb.AddForce(Vector3.forward * Speed * Time.fixedDeltaTime);
-                IsMoving = true;
-            }
-            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            {
-                Rb.AddForce(Vector3.back * Speed * Time.fixedDeltaTime);
-                IsMoving = true;
-            }
-
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
-                Rb.AddForce(Vector3.left * Speed * Time.fixedDeltaTime);
-                IsMoving = true;
-            }
-            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
-                Rb.AddForce(Vector3.right * Speed * Time.fixedDeltaTime);
-                IsMoving = true;
-            }
-        }
+        Rb.AddForce(dir * Time.fixedDeltaTime * Speed * 3f);
     }
 
     void OnCollisionEnter(Collision collision)
--- Assets/Scripts/CharacterFollow.cs
+++ Assets/Scripts/CharacterFollow.cs
@@ -4,45 +4,110 @@ public class CharacterFollow : MonoBehaviour
 {
     public Animator Animator;
     public BumperBall Ball;
-    public float Speed = 50;
-    public bool IsRotated;
+    public GameObject Character;
+    public bool IsPlayer;
+    public bool IsMoving;
 
-    private Vector3 _offset;
+    private Vector3 _characterOffset;
     private bool _startedMoving;
-    private int _multiplier;
+    private Vector3 _forceToAdd = Vector3.zero;
+    private BumperBall _targetBall;
 
     void Start()
     {
-        _offset = transform.position - Ball.transform.position;
-        _multiplier = IsRotated ? -1 : 1;
+        _characterOffset = Character.transform.position - Ball.transform.position;
     }
 
     void Update()
     {
-        if (!_startedMoving && Ball.IsMoving)
+        if (!_startedMoving && IsMoving)
         {
             Animator.SetInteger("State", (int)CharacterState.Walk);
             _startedMoving = true;
         }
 
-        if (Ball.IsPlayer)
+        _forceToAdd = Vector3.zero;
+
+        var remainingCharacters = FindObjectsOfType<CharacterFollow>();
+        if (remainingCharacters.Length == 1)
         {
+            Ball.Rb.isKinematic = true;
+        }
+
+        if (IsPlayer)
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                _forceToAdd = Vector3.forward;
+                IsMoving = true;
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                _forceToAdd = Vector3.back;
+                IsMoving = true;
+            }
+
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Rotate(new Vector3(0, -Speed * _multiplier, 0) * Time.deltaTime, Space.Self);
+                _forceToAdd += Vector3.left;
+                IsMoving = true;
             }
             else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                transform.Rotate(new Vector3(0, Speed * _multiplier, 0) * Time.deltaTime, Space.Self);
+                _forceToAdd += Vector3.right;
+                IsMoving = true;
+            }
+        }
+        else
+        {
+            var bumperBalls = FindObjectsOfType<BumperBall>();
+            BumperBall closestBumperBall = null;
+            float closestDistance = Mathf.Infinity;
+            foreach (BumperBall bumperBall in bumperBalls)
+            {
+                if (bumperBall == Ball)
+                {
+                    continue;
+                }
+                var distance = Vector3.Distance(transform.position, bumperBall.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestBumperBall = bumperBall;
+                    _targetBall = closestBumperBall;
+                }
+            }
+
+            if (closestBumperBall == null)
+            {
+                _forceToAdd = Vector3.zero;
+            }
+            else
+            {
+                var diff = (closestBumperBall.transform.position - Ball.transform.position).normalized;
+                _forceToAdd = diff;
             }
         }
     }
 
     void LateUpdate()
     {
-        if (Ball != null)
+        Character.transform.position = Ball.transform.position + _characterOffset;
+        if (_targetBall != null)
         {
-            transform.position = Ball.transform.position + _offset;
+            Character.transform.LookAt(new Vector3(_targetBall.transform.position.x, Character.transform.position.y, _targetBall.transform.position.z));
+        }
+        else if (IsPlayer)
+        {
+            Character.transform.LookAt(new Vector3(_forceToAdd.x * 10, Character.transform.position.y, _forceToAdd.z * 10));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_forceToAdd != Vector3.zero)
+        {
+            Ball.AddForce(_forceToAdd);
         }
     }
 }
